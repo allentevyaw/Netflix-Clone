cb96773c0931274e2538106fe3937a99c90e7112
allentevyaw <atevyaw@gmail.com>
2023-01-11 20:50:24 -0500

    row buttons added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <div>
         <Main />
-        <Row title='Upcoming' fetchURL={requests.requestUpcoming}/>
-        <Row title='Latest Movies' fetchURL={requests.requestLatest}/>
-        <Row title='Top Rated' fetchURL={requests.requestTopRated}/>
-        <Row title='Trending' fetchURL={requests.requestTrending}/>
+        <Row rowID='1' title='Upcoming' fetchURL={requests.requestUpcoming}/>
+        <Row rowID='2' title='Latest Movies' fetchURL={requests.requestLatest}/>
+        <Row rowID='3' title='Top Rated' fetchURL={requests.requestTopRated}/>
+        <Row rowID='4' title='Trending' fetchURL={requests.requestTrending}/>
     </div>
   )
 }

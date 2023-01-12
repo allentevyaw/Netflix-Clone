fda666da6dbe06fe2332865c694d03cb9125eecf
allentevyaw <atevyaw@gmail.com>
2023-01-11 21:41:19 -0500

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { AuthContextProvider } from './context/AuthContext'
 import Home from './pages/Home'
 
 function App() {
 
   return (
     <div>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
+      <AuthContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+      </AuthContextProvider>
     </div>
   )
 }

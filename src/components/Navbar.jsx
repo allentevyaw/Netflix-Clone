8cc38875b417d2a0241dafbf5ec6d8a3523ee285
allentevyaw <atevyaw@gmail.com>
2023-01-12 21:10:40 -0500

    firebase changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext' 
 
 const Navbar = () => {
+const {user, loutOut} = UserAuth()
+const navigate = useNa
+
+const handleLout = async () => {
+
+  try {
+    await logOut()
+    
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+
   return (
     <div className='flex items-center justify-between w-full p-4 z-[100] absolute'>
       <Link to='/'>
@@ -9,14 +24,25 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link> 
-        <div>
-          <Link to='/login'> 
-            <button className='text-white pr-4'>Sign In</button>
-          </Link>
-          <Link to='/signup'>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointe text-whiter'>Sign Up </button>
-          </Link>
-        </div>
+      {user?.email ?
+              <div>
+              <Link to='/account'> 
+                <button className='text-white pr-4'>Account</button>
+              </Link>
+              <Link to='/signup'>
+                <button className='bg-red-600 px-6 py-2 rounded cursor-pointe text-whiter'>
+                  Logout </button>
+              </Link>
+            </div> :
+                    <div>
+                    <Link to='/login'> 
+                      <button className='text-white pr-4'>Sign In</button>
+                    </Link>
+                    <Link to='/signup'>
+                      <button className='bg-red-600 px-6 py-2 rounded cursor-pointe text-whiter'>
+                        Sign Up </button>
+                    </Link>
+                  </div>}
     </div>
   )
 }

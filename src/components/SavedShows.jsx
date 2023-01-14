cddf89ba95576aa3546fdb398b1848d80c159879
allentevyaw <atevyaw@gmail.com>
2023-01-14 08:50:57 -0500

    delete user

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -4,12 +4,14 @@ import { UserAuth } from '../context/AuthContext'
 import { db } from '../firebase.config'
 import {updateDoc, doc, onSnapshot} from 'firebase/firestore'
 import {AiOutlineClose} from 'react-icons/ai'
+import { async } from '@firebase/util'
 
 
 const SavedShows = () => {
 
 const [movies, setMovies] = useState([])
 const {user} = UserAuth()
+const movieRef = doc(db, 'users', `${user?.email}`)
 
 
 const slideLeft = () => {
@@ -29,6 +31,14 @@ useEffect(() => {
 
 }, [user?.email])
 
+const deleteShow =  async (pastid) => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
   return (
     <>
         <h2 className='text-white font-bold md:text-xl p-4'> My Shows</h2> 
@@ -36,23 +46,29 @@ useEffect(() => {
             <MdChevronLeft 
                 onClick={slideLeft} 
                 size={40} 
-                className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
+                className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+            />
         <div 
             className='full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative' 
-            id={'slider'}>
+            id={'slider'}
+        >
                 {movies.map((item, id) => (
                         <div key={id} className='w-[160px] sm:w-[200px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                        <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.img}`} alt={item?.title} />
+                        <img 
+                            className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.img}`} 
+                            alt={item?.title} 
+                        />
                         <div className='absolute top-0 left-o w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                             <p className='text-xs whitespace-pre-line md:text-sm font-bold flex justify-center items-center h-full text-center p-4'>{item?.title}</p>
                             <p className='absolute text-grey-300 top-4 right-4'><AiOutlineClose /></p>
                         </div>
-                    </div>
+                        </div>
                 ))}
         </div>
             <MdChevronRight onClick={slideRight} 
                 size={40} 
                 className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'/>
+                onClick={() => deleteShow(item.ID)}
        </div>
     </>
   )
